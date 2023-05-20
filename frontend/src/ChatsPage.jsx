14ed318ea266544f698ab4bbcf3be28717daab0e
Jerry Lee <hyunsuk.lee1@gmail.com>
2023-05-20 18:59:02 -0400

    UI commit

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,18 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic} from 'react-chat-engine-advanced'
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(
+        '27e44235-493c-4d9d-8072-72b0189f5f0d',
+        props.user.username,
+        props.user.secret
+    );
+    
+    return (
+        <div style= {{ height: '100vh'}}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{ height: '100%'}} />
+        </div>    
+    )
 }
 
 export default ChatsPage
